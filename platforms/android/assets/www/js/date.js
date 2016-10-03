b1220f597d32103814db041215279e3205592a61
layely <layely@layely-HP-650PC>
2016-10-03 04:17:13 +0000

    add logo and spash screen

--- platforms/android/assets/www/js/date.js
+++ platforms/android/assets/www/js/date.js
@@ -1,7 +1,6 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Propose le service getDate() 
+ * qui retourne la date et heure courante du téléphone
  */
 
 var dateStr = '';
